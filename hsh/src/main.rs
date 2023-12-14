db04c6f003f631e642e63a913c32895772d95a38
HE7086 <me@heyi7086.com>
2023-12-14 11:08:12 +0100

    rename using underscore

--- hsh/src/main.rs
+++ hsh/src/main.rs
@@ -1,3 +1,15 @@
+use hsh_parse;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(trailing_var_arg=true)]
+#[command(version)]
+struct Args {
+
+}
+
 fn main() {
+    let args = Args::parse();
     println!("Hello, world!");
 }
