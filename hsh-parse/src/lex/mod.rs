9d80e7be2617174b62aed9d436b3801e1ef363db
HE7086 <me@heyi7086.com>
2023-12-14 07:00:55 +0100

    use match

--- hsh-parse/src/lex/mod.rs
+++ hsh-parse/src/lex/mod.rs
@@ -93,103 +93,99 @@ impl<'a> Iterator for Lexer<'a> {
     fn next(&mut self) -> Option<Self::Item> {
         let mut ctx = LexerContext::new();
         let mut result = Vec::<(usize, char)>::new();
-        while self.chars.next_if(|&(_, c)| c.is_whitespace()).is_some() {}
         while let Some(&(_, c)) = self.chars.peek() {
             // If the end of input is recognized, the current token (if any) shall be delimited.
-            if ctx.operator && !ctx.quoted() && form_valid_operator(self.get_str(&result), c) {
-                // If the previous character was used as part of an operator and the current character
-                // is not quoted and can be used with the previous characters to form an operator,
-                // it shall be used as part of that (operator) token.
-                result.push(self.chars.next().unwrap());
-                continue;
-            }
-            if ctx.operator && !form_valid_operator(self.get_str(&result), c) {
-                // If the previous character was used as part of an operator and
-                // the current character cannot be used with the previous characters to form an operator,
-                // the operator containing the previous character shall be delimited.
-                break;
-            }
-            if "\\'\"".contains(c) && !ctx.quoted() {
-                // If the current character is <backslash>, single-quote, or double-quote and it is not quoted,
-                // it shall affect quoting for subsequent characters up to the end of the quoted text.
-                // The rules for quoting are as described in Quoting.
-                // During token recognition no substitutions shall be actually performed,
-                // and the result token shall contain exactly the characters
-                // that appear in the input (except for <newline> joining), unmodified,
-                // including any embedded or enclosing quotes or substitution operators,
-                // between the <quotation-mark> and the end of the quoted text.
-                // The token shall not be delimited by the end of the quoted field.
-                ctx.quotes.push(c);
-                result.push(self.chars.next().unwrap());
-                continue;
-                //todo
-            }
-            if "\\'\"".contains(c) && ctx.quoted() && ctx.quote() == c {
-                ctx.quotes.pop();
-                result.push(self.chars.next().unwrap());
-                continue;
-            }
-            if "$`".contains(c) && !ctx.quoted() {
-                // If the current character is an unquoted '$' or '`',
-                // the shell shall identify the start of any candidates
-                // for parameter expansion (Parameter Expansion),
-                // command substitution (Command Substitution), or arithmetic expansion (Arithmetic Expansion)
-                // from their introductory unquoted character sequences:
-                // '$' or "${", "$(" or '`', and "$((", respectively.
-                // The shell shall read sufficient input to determine
-                // the end of the unit to be expanded (as explained in the cited sections).
-                // While processing the characters,
-                // if instances of expansions or quoting are found nested within the substitution,
-                // the shell shall recursively process them in the manner specified for the construct that is found.
-                // The characters found from the beginning of the substitution to its end,
-                // allowing for any recursion necessary to recognize embedded constructs,
-                // shall be included unmodified in the result token,
-                // including any embedded or enclosing substitution operators or quotes.
-                // The token shall not be delimited by the end of the substitution.
-
-                // TODO
-                continue;
-            }
-            if is_operator_begin(c) && !ctx.quoted() {
-                // If the current character is not quoted and can be used as the first character of a new operator,
-                // the current token (if any) shall be delimited.
-                // The current character shall be used as the beginning of the next (operator) token.
-                if result.is_empty() {
-                    ctx.operator = true;
+            match c {
+                _ if ctx.operator && !ctx.quoted() && form_valid_operator(self.get_str(&result), c) => {
+                    // If the previous character was used as part of an operator and the current character
+                    // is not quoted and can be used with the previous characters to form an operator,
+                    // it shall be used as part of that (operator) token.
                     result.push(self.chars.next().unwrap());
-                    continue;
-                } else {
+                }
+                _ if ctx.operator && !form_valid_operator(self.get_str(&result), c) => {
+                    // If the previous character was used as part of an operator and
+                    // the current character cannot be used with the previous characters to form an operator,
+                    // the operator containing the previous character shall be delimited.
                     break;
                 }
-            }
-            if c.is_whitespace() && !ctx.quoted() {
-                // If the current character is an unquoted <blank>,
-                // any token containing the previous character is delimited
-                // and the current character shall be discarded.
-                self.chars.next();
-                break;
-            }
-            if !result.is_empty() {
-                // If the previous character was part of a word,
-                // the current character shall be appended to that word.
-                result.push(self.chars.next().unwrap());
-                if ctx.quoted() && ctx.quote() == '\\' {
-                    ctx.quotes.pop();
+                '\\' | '\'' | '"' if !ctx.quoted() => {
+                    // If the current character is <backslash>, single-quote, or double-quote and it is not quoted,
+                    // it shall affect quoting for subsequent characters up to the end of the quoted text.
+                    // The rules for quoting are as described in Quoting.
+                    // During token recognition no substitutions shall be actually performed,
+                    // and the result token shall contain exactly the characters
+                    // that appear in the input (except for <newline> joining), unmodified,
+                    // including any embedded or enclosing quotes or substitution operators,
+                    // between the <quotation-mark> and the end of the quoted text.
+                    // The token shall not be delimited by the end of the quoted field.
+                    ctx.quotes.push(c);
+                    result.push(self.chars.next().unwrap());
+                    //todo
                 }
-                continue;
-            }
-            if '#' == c {
-                // If the current character is a '#', it and all subsequent characters up to,
-                // but excluding, the next <newline> shall be discarded as a comment.
-                // The <newline> that ends the line is not considered part of the comment.
-                ctx.comment = true;
-                while self.chars.next_if(|&(_, c)| c != '\n').is_some() {}
-                continue;
-            }
-            {
-                // The current character is used as the start of a new word.
-                result.push(self.chars.next().unwrap());
-                continue;
+                '\\' | '\'' | '"' if ctx.quoted() && ctx.quote() == c => {
+                    ctx.quotes.pop();
+                    result.push(self.chars.next().unwrap());
+                }
+                '$' | '`' if !ctx.quoted() => {
+                    // If the current character is an unquoted '$' or '`',
+                    // the shell shall identify the start of any candidates
+                    // for parameter expansion (Parameter Expansion),
+                    // command substitution (Command Substitution), or arithmetic expansion (Arithmetic Expansion)
+                    // from their introductory unquoted character sequences:
+                    // '$' or "${", "$(" or '`', and "$((", respectively.
+                    // The shell shall read sufficient input to determine
+                    // the end of the unit to be expanded (as explained in the cited sections).
+                    // While processing the characters,
+                    // if instances of expansions or quoting are found nested within the substitution,
+                    // the shell shall recursively process them in the manner specified for the construct that is found.
+                    // The characters found from the beginning of the substitution to its end,
+                    // allowing for any recursion necessary to recognize embedded constructs,
+                    // shall be included unmodified in the result token,
+                    // including any embedded or enclosing substitution operators or quotes.
+                    // The token shall not be delimited by the end of the substitution.
+
+                    // TODO
+                }
+                _ if is_operator_begin(c) && !ctx.quoted() => {
+                    // If the current character is not quoted and can be used as the first character of a new operator,
+                    // the current token (if any) shall be delimited.
+                    // The current character shall be used as the beginning of the next (operator) token.
+                    if result.is_empty() {
+                        ctx.operator = true;
+                        result.push(self.chars.next().unwrap());
+                    } else {
+                        break;
+                    }
+                }
+                _ if c.is_whitespace() && !ctx.quoted() => {
+                    // If the current character is an unquoted <blank>,
+                    // any token containing the previous character is delimited
+                    // and the current character shall be discarded.
+                    self.chars.next();
+                    if !result.is_empty() {
+                        break;
+                    }
+                }
+                _ if !result.is_empty() => {
+                    // If the previous character was part of a word,
+                    // the current character shall be appended to that word.
+                    result.push(self.chars.next().unwrap());
+                    if ctx.quoted() && ctx.quote() == '\\' {
+                        ctx.quotes.pop();
+                    }
+                }
+                '#' => {
+                    // If the current character is a '#', it and all subsequent characters up to,
+                    // but excluding, the next <newline> shall be discarded as a comment.
+                    // The <newline> that ends the line is not considered part of the comment.
+                    ctx.comment = true;
+                    while self.chars.next_if(|&(_, c)| c != '\n').is_some() {}
+                }
+                _ => {
+                    // The current character is used as the start of a new word.
+                    debug_assert!(result.is_empty());
+                    result.push(self.chars.next().unwrap());
+                }
             }
         }
         if result.is_empty() {
